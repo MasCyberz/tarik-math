6cde1eecd3152d18c2d102402d64294ade20da18
Dimas Alif Ardiansyah <dimasned20@gmail.com>
2025-11-21 08:26:39 +0700

    kasih link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <div className="p-6 text-center text-gray-600 fixed bottom-0 left-0 w-full">
          <p>
             Copyright &copy; {new Date().getFullYear()} Tarik Matematika. Made
-            by Dimas with <span className="text-red-500">❤️</span>
+            by <span><a href="https://www.instagram.com/dimsalif_" className="text-blue-300">Dimas</a></span> with <span>❤️</span>
          </p>
       </div>
    );
